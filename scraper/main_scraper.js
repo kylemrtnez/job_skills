a01f97d8c3db6139059558e8f2e1d2942948c9d0
kylemrtnez <kmartinez7789@gmail.com>
2018-09-14 14:50:53 -0500

    Add initial implementation of search page scraper
    Adds 4 functions which implement the scraper for job search pages. This
    includes getting html, scraping initial information, and storing it in
    an array.

--- scraper/main_scraper.js
+++ scraper/main_scraper.js
@@ -43,34 +43,118 @@ const getJobSearchUrls = function generateArrayOfJobSearchPages(title, loc, url)
 const searchesToRequest = getJobSearchUrls(jobTitle, location, baseUrl);
 
 
-console.log(searchesToRequest);
-
-
-
-
-// how to view a specific job page:
-// 'https://www.indeed.com/viewjob?jk=' random chars
-// const createJobSearchUrl = function constructJobSearchUrl(title, loc) {
-
-//   const urlifyJobTitle = function addPlusSignsToJobTitleForSearchString(localTitle) {
-//     return `q=${localTitle.split(' ').join('+')}`;
-//   };
-
-//   const urlifyLocation = function urlifyLocationForSearchString(localLocation) {
-//     return `l=${localLocation.split(' ').join('+')}`;
-//   };
-
-//   return `${baseUrl}/jobs?${urlifyJobTitle(title)}&${urlifyLocation(loc)}`;
-// };
-
-// const crawlForward = function crawlPageForward(url, val) {
-//   return `${url}&start=${val}`;
-// };
-
-
-
-
 // Take Array of URLs, scrape info off them from each posting. Populates object of job info
+
+const asyncGetHtml = async function asyncFetchtoHtml(url) {
+  try {
+    const response = await fetch(url);
+    const responseHtml = await response.text();
+    return responseHtml;
+  } catch (e) {
+    throw new Error('Fetch failed.');
+  }
+};
+
+
+const scrapeJobInfo = function scrapeJobSearchResults(html) {
+  const scrubCompany = function scrubScrapedCompanyName(companyString) {
+    return companyString.trim();
+  };
+
+  const scrubLocation = function scrubScrapedLocation(locationString) {
+    const locationRe = /^.*[A-Z]{2}/;
+    return locationString.trim().match(locationRe)[0];
+  };
+
+  const arrayOfScrapedInfo = [];
+  const $ = cheerio.load(html);
+  const indeedJobKeyField = 'data-jk';
+  const indeedLocationHtmlClass = '.location';
+  const indeedCompanyNameHtmlClass = '.company';
+
+  $(`[${indeedJobKeyField}]`).each((i, element) => {
+    const jobStuff = {};
+    jobStuff.jobKey = element.attribs[indeedJobKeyField];
+
+    const rawLocation = $(element).find(indeedLocationHtmlClass).text()
+    jobStuff.location = scrubLocation(rawLocation);
+
+    const rawCompanyName = $(element).find(indeedCompanyNameHtmlClass).text()
+    jobStuff.companyName = scrubCompany(rawCompanyName);
+
+    arrayOfScrapedInfo.push(jobStuff);
+  });
+
+  return arrayOfScrapedInfo;
+};
+
+const getJobInfo = async function requestAndScrapeInfo(singleSearchUrl) {
+  // return scrapeJobInfo(await asyncGetHtml(searchUrl));
+  const jobInfoHtml = await asyncGetHtml(singleSearchUrl);
+  const jobInfo = scrapeJobInfo(await jobInfoHtml);
+  return jobInfo;
+};
+
+let globalJobInfo = [];
+let scraperIdx = 1;
+const numPagesToScrape = 3;
+
+const firstStageScrape = function firstStageScrapeAndCombineResults(allSearchUrls) {
+  getJobInfo(allSearchUrls[scraperIdx])
+    .then((info) => {
+      globalJobInfo = globalJobInfo.concat(info);
+      scraperIdx += 1;
+    })
+    .then(() => {
+      if (scraperIdx < numPagesToScrape) {
+        firstStageScrape(allSearchUrls);
+      }
+    });
+};
+
+firstStageScrape(searchesToRequest);
+setTimeout(() => console.log(globalJobInfo), 3000);
+// const scrapeJobInfo = async function scrapeJobSearchResults(urls) {
+//   const requestsToMake = searchesToRequest.length;
+
+//   fetch(url)
+//     .then(res => res.text())
+//     .then((html) => {
+//       const $ = cheerio.load(html);
+//       const indeedJobKeyField = 'data-jk';
+//       const indeedLocationHtmlClass = '.location';
+
+//       const selectHtmlBy = function selectHtmlDataBySelector(selector) {
+//         return $(`${selector}`);
+//       };
+
+//       const rawJobInfo = selectHtmlBy(`[${indeedJobKeyField}]`);
+//       // console.log(rawJobInfo);
+
+//       const rawLocationInfo = selectHtmlBy(indeedLocationHtmlClass);
+//       console.log(rawLocationInfo.contents().text());
+
+//       // extract job keys to create specific job page links
+//       rawJobInfo.each((i, element) => {
+//         extractedJobInfo.push(element.attribs[indeedJobKeyField]);
+//       });
+
+//       // console.log(extractedJobInfo);
+//     })
+//     // .then(() => {
+//     //   if (idx < 3) {
+//     //     idx += 1;
+//     //     const newCrawlVal = crawlVal + 10;
+//     //     const nextUrl = crawlForward(url, newCrawlVal);
+//     //     console.log(nextUrl);
+//     //     getJobLinks(nextUrl, newCrawlVal);
+//     //   }
+//     // })
+//     .catch(error => console.error(error));
+// };
+// console.log(searchesToRequest);
+
+
 
 
 // For each job posting URL, visit and scrape job name and skills. Populate object of job info
